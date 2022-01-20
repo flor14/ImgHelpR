dadf77d645c10c85a98442c450c8c72b9896a54f
MaeveShi <59086617+MaeveShi@users.noreply.github.com>
2022-01-19 21:26:15 -0800

    docs: Create Crop.R

---
+++ R/Crop.R
@@ -0,0 +1,27 @@
+#' Title
+#'
+#' @param input_path  ----String, path of the input image
+#' @param output_path ----String, path of the output image
+#' @param height ----Int, height of the desired image
+#' @param width  ----Int, width of the desried image
+#'
+#' @return
+#' @export
+#' @import numbers
+#' @import R.utils
+#' @import png
+#' @import OpenImageR
+#' @import here
+#' @import testthat
+#' @import reticulate
+#' @import rprojroot
+#' @import assertthat
+#' @import covr
+#' @examples
+#' # Crop("input_path", "output_path", 10L, 10L)
+library(OpenImageR)
+library(numbers)
+library(png)
+Crop <- function(input_path, output_path, height, width){
+  TRUE
+ }
