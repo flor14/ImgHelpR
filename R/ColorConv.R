35ed6fcea2319f8c715ef9d1b44eed115ecd9f8d
sufang <sufangtan.ca@gmail.com>
2022-01-20 17:44:12 -0800

    add empty ColorConv function

---
+++ R/ColorConv.R
@@ -0,0 +1,14 @@
+#'Image Color Conversion
+#'
+#'This function converts an image to one of the color : gray, red, green, blue
+#'
+#' @param img path of the input image from package jpeg's function readJPEG().
+#' @param color desired color to convert: gray, red, green, blue
+#'
+#' @return The converted image
+#' @examples
+#' ColorConv(img, color = 'gray')
+
+ColorConv <- function(img, color){
+  TRUE
+}
