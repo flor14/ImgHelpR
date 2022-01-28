6c5e25050a2bead80718fb95d10bde1f67c5c72d
jasmineortega <56210631+jasmineortega@users.noreply.github.com>
2022-01-28 14:43:48 -0800

    Edit function names in testthat statements
    Oops

--- tests/testthat/test-ImgRotate.R
+++ tests/testthat/test-ImgRotate.R
@@ -7,7 +7,7 @@ test_that("Error should be thrown when input is not array type", {
 wrong_dim <- array(c(1:15),dim = c(3,3,0))
 
 test_that("Error should be thrown when array is not 3D/RGB image!",{
-    expect_error(ImgCompress(wrong_dim,360))
+    expect_error(ImgRotate(wrong_dim,360))
 })
 
 
@@ -15,10 +15,10 @@ test_that("Error should be thrown when array is not 3D/RGB image!",{
 wrong_size <- array(c(1:15),dim = c(0,0,0))
 
 test_that("Error should be thrown when array is not 3D/RGB image!",{
-    expect_error(ImgCompress(wrong_dim ,360))
+    expect_error(ImgRotate(wrong_dim ,360))
 })
 
 # test for incorrect degree input
 test_that("Error should be thrown when degree is not in [90, 180, 270, 360]", {
-  expect_error(ImgCompress(img,299.4))
+  expect_error(ImgRotate(img,299.4))
 })
